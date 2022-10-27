86dcbc24943f4b31c4d4318c69639f3cab307aaa
Joseph Del Giorgio <106563909+Joseph-DelGiorgio@users.noreply.github.com>
2022-10-27 16:48:40 -0700

    Create deploy.js

---
+++ deploy.js
@@ -0,0 +1,52 @@
+const { ethers } = require("hardhat");
+
+async function main(){
+    [signer1, signer2] = await ethers.getSigners();
+
+    const Staking= await ethers.getContractFactory('Staking', signer1);
+
+    staking= await Staking.deploy({
+        value: ethers.utils.parseEther('10')
+    });
+
+    console.log("staking contract deployed to:", staking.address, "by", signer1.address)
+
+    const provider = waffle.provider;
+    let data;
+    let transaction;
+    let reciept;
+    let block;
+    let newUnlockDate;
+
+    data= {value: ethers.utils.parseEther('0.5') }
+    transaction= await staking.connect(signer2).stakeEther(30, data)
+
+    data= {value: ethers.utils.parseEther('1') }
+    transaction= await staking.connect(signer2).stakeEther(180, data)
+
+    data= {value: ethers.utils.parseEther('1.75') }
+    transaction= await staking.connect(signer2).stakeEther(180, data)
+
+    data= {value: ethers.utils.parseEther('5') }
+    transaction= await staking.connect(signer2).stakeEther(90, data)
+    reciept= await transaction.wait()
+    block= await provider.getBlock(reciept.blockNumber)
+    newUnlockDate= block.timestamp - (60 * 60 * 24 * 100)
+    await staking.connect(signer1).changeUnlockDate(3, newUnlockDate)
+
+    data= {value: ethers.utils.parseEther('1.75') }
+    transaction= await staking.connect(signer2).stakeEther(180, data)
+    reciept= await transaction.wait()
+    block= await provider.getBlock(reciept.blockNumber)
+    newUnlockDate= block.timestamp - (60 * 60 * 24 * 100)
+    await staking.connect(signer1).changeUnlockDate(4, newUnlockDate)
+}
+
+// npx hardhat run --network localhost scripts/deploy.js
+
+main()
+    .then(()=> process.exit(0))
+    .catch((error)=>{
+        console.error(error);
+        process.exit(1);
+    });
